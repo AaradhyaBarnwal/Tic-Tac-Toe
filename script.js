622cb78ebd941f8183abe85971dcbf268c278c1b
Aaradhya Barnwal <aaradhyabarnwal82@gmail.com>
2026-03-27 13:33:21 +0530

    added some changes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let boxTag = document.querySelectorAll(".box");
 let resetBtn = document.querySelector('.Reset');
+let winnerTag = document.querySelector('.winner');
+let conTag = document.querySelector(".con");
+
 
 let turnO = true;
 
@@ -18,10 +21,14 @@ boxTag.forEach((box)=> {
     box.addEventListener('click', () =>{
         console.log(2);
     if(turnO){
-        box.innerText = 'O';
+        box.innerText="X";
+//         box.innerHTML= `
+// <img src="assets/cat.png" class="cats" alt="">
+//         `;
         turnO = false;
     }else{
-        box.innerText = 'X';
+        box.innerText='O';
+    //    box.innerHTML=`<h1>O</h1>`;
         turnO = true;
     }
     box.disabled = true;
@@ -31,6 +38,35 @@ boxTag.forEach((box)=> {
     })
 }) ;
 
+const winner = () => {
+    for(pattern of winPatterns){
+        
+        
 
+                let pos1val = boxTag[pattern[0]].innerText;
+                let pos2val = boxTag[pattern[1]].innerText;
+                let pos3val = boxTag[pattern[2]].innerText;
+            
+                console.log(1);
+
+                if (pos1val !== '' && pos2val !== '' && pos3val !== '') {
+               console.log(2);
+                 
+                    if (pos1val === pos2val && pos2val === pos3val) {
+                        console.log(3);
+                        alert('won');
+                
+                        
+
+                    }
+                }
+            }
+}
+
+resetBtn.addEventListener('click',function () {
+    turnO=true;
+}
+
+)
 
 console.log(1);
