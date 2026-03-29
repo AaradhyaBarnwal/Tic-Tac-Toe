477df4c7b7d89f9ee741e08254e07ed4e57b2113
Aaradhya Barnwal <aaradhyabarnwal82@gmail.com>
2026-03-30 00:27:24 +0530

    finished the project

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 <h1>Tic Tac Toe game</h1>
+
+<h2>Why I made it:</h2>
+<p>I made it because I wanted to play tic tac toe.</p>
+
+<h2>What I struggled with and what I learned:</h2>
+<p>I struggled with making the winner tag and using the reset button.</p>
+<p>I learned how to use css in javascript and I used a lot  of new tags .</p>
+
+<h2>How to use it :<h2>
+<p>Click in one of the boxes and play tic tac toe just like we play in pen and paper.</p>
+<p>To start a new game click the reset or new game button.</p>
+
+<h3>Quick note: The winner and draw card will be gone as you will click the new game button.</h3>
+
+<h2>What is this?</h2>
+<p>It is a web app where we can play tic tac toe.</p>
+
+<h2>How did I made it ?</h2>
+<p>I made it using css , html and javascript in vs code.</p>
+<p>I have also made some custom pixel icons using pixel art (I recorded it using lapse).</p>
+<p>I have used google fonts for the fonts.</p>
--- script.js
+++ script.js
@@ -6,116 +6,115 @@ let playerTurnTag = document.querySelector('.abe');
 let turnO = true;
 
 const winPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
 ];
 
-boxTag.forEach((box)=> {
-    box.addEventListener('click', () =>{
+boxTag.forEach((box) => {
+    box.addEventListener('click', () => {
         console.log(2);
-    if(turnO){
-        box.innerText="X";
-//         box.innerHTML= `
-// <img src="assets/cat.png" class="cats" alt="">
-//         `;
-        turnO = false;
+        if (turnO) {
+            box.innerHTML = `<h4 class="X">X</h4>`;
+
+            turnO = false;
             chalNa("O");
-    }else{
-        box.innerText='O';
-    //    box.innerHTML=`<h1>O</h1>`;
-        turnO = true;
-        chalNa('X');
-    }
-    box.disabled = true;
+        } else {
+            box.innerHTML = `<h4 class="O">O</h4>`;
+            //    box.innerHTML=`<h1>O</h1>`;
+            turnO = true;
+            chalNa('X');
+        }
+        box.disabled = true;
 
 
-    winner();
-    draw();
+        winner();
+        draw();
 
     })
-}) ;
+});
 
 const winner = () => {
-    for(pattern of winPatterns){
-        
-        
+    for (pattern of winPatterns) {
 
-                let pos1val = boxTag[pattern[0]].innerText;
-                let pos2val = boxTag[pattern[1]].innerText;
-                let pos3val = boxTag[pattern[2]].innerText;
-            
-                console.log(1);
 
-                if (pos1val !== '' && pos2val !== '' && pos3val !== '') {
-               console.log(2);
-                 
-                    if (pos1val === pos2val && pos2val === pos3val) {
-                        console.log(3);
-                        winnerTag.innerHTML=`<h2>${pos1val} player<h2>You won the game🌸</h2>
-                        <h2>Congratulations</h2>
+
+        let pos1val = boxTag[pattern[0]].innerText;
+        let pos2val = boxTag[pattern[1]].innerText;
+        let pos3val = boxTag[pattern[2]].innerText;
+
+        console.log(1);
+
+        if (pos1val !== '' && pos2val !== '' && pos3val !== '') {
+            console.log(2);
+
+            if (pos1val === pos2val && pos2val === pos3val) {
+                console.log(3);
+                winnerTag.innerHTML = `<h2>${pos1val} player Wins!!</h2>
+                        <h2 class="flower">🌸</h2>
+                        <h2 class="congo">Congratulations</h2>
                           <img src="assets/cat.png" class="cats" alt=""> </h2>`
-                        winnerTag.style.display="grid";
-        disableAllBoxes();
-                        damn();
+                winnerTag.style.display = "grid";
+                disableAllBoxes();
+                damn();
 
-                    }else {}
+            } else { }
 
 
-                }
+        }
 
-            }
+    }
 }
 console.log(1);
 
-const resetGame = () =>{
+const resetGame = () => {
     turnO = true;
 
-    boxTag.forEach(box =>{
-            box.innerText='';
-            box.disabled=false;
-        
-        });
-        winnerTag.innerHTML=``;
-        winnerTag.style.display='none';
-            playerTurnTag.innerHTML=`<h3>X's player turn</h3>`;
-        damn();
+    boxTag.forEach(box => {
+        box.innerText = '';
+        box.disabled = false;
+
+    });
+    winnerTag.innerHTML = ``;
+    winnerTag.style.display = 'none';
+    playerTurnTag.innerHTML = `<h3>X's player turn</h3>`;
+    damn();
 
 }
 
-resetBtn.addEventListener("click",resetGame)
+resetBtn.addEventListener("click", resetGame)
 
 function gurl() {
-    resetBtn.innerHTML=`<p>Reset</p>`
+    resetBtn.innerHTML = `<p>Reset</p>`
 }
 
-function damn () {
-    if ( winnerTag.style.display==="grid") {
-    resetBtn.innerHTML=`<p>New Game</p>`;
-} else if(winnerTag.style.display==="none") {
-    resetBtn.innerHTML=`<p>Reset</p>`;
-}
+function damn() {
+    if (winnerTag.style.display === "grid") {
+        resetBtn.innerHTML = `<p>New Game</p>`;
+    } else if (winnerTag.style.display === "none") {
+        resetBtn.innerHTML = `<p>Reset</p>`;
+    }
 
-}                       
+}
 const draw = () => {
 
-    if (winnerTag.style.display === 'grid') {return;} 
+    if (winnerTag.style.display === 'grid') { return; }
 
     let allFilled = [...boxTag].every(box => box.innerText != '')
 
     if (allFilled) {
-        winnerTag.innerHTML=`
+        winnerTag.innerHTML = `
         <h2>It's a Draw!🤝</h2>
         
         <h2>Try Again</h2>  
         
         `
-        ;
+            ;
 
         winnerTag.style.display = "grid";
         disableAllBoxes();
@@ -126,9 +125,9 @@ const draw = () => {
 }
 
 function chalNa(players) {
-    playerTurnTag.innerHTML=`<h3>Player ${players}'s turn</h3>`;
+    playerTurnTag.innerHTML = `<h3>Player <span> ${players}</span>'s turn</h3>`;
 }
 
 const disableAllBoxes = () => {
-boxTag.forEach(box => box.disabled = true);
+    boxTag.forEach(box => box.disabled = true);
 };
